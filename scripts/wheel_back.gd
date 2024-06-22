d692a279c89eaebf845eebdae5a5165dfa4dc914
javi <javierrcalvete@gmail.com>
2024-06-22 17:55:46 -0300

    added wheel sprites and function

---
+++ scripts/wheel_back.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+var selected = false
+
+# variable para saber si se selecciono el collision shape, escucha solos los del collision
+func _on_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("click"):
+		selected = true
+		
+		
+# lerp agarra 2 valores y lo interpola con la velocidad del 3 valor
+func _physics_process(delta):
+	if selected:
+		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
+
+# generic input events on scripts, captura todos los input events
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+			selected = false
