4a19dbc8f58e83c108004a5b15db330c4b23380b
javi <javierrcalvete@gmail.com>
2024-06-23 15:57:15 -0300

    made saddle stay in position on bike/cant click it again

--- scripts/saddle.gd
+++ scripts/saddle.gd
@@ -1,15 +1,20 @@
 extends Area2D
 @onready var game_manager = %GameManager
-	
+
+var saddle_in_position = false
+
 func _ready():
 	$Label.hide()
+	
 
 
 var selected = false
 
 # variable para saber si se selecciono el collision shape, escucha solos los del collision
 func _on_input_event(viewport, event, shape_idx):
-	if Input.is_action_just_pressed("click") and game_manager.is_mouse_busy == false:
+	if Input.is_action_just_pressed("click") and game_manager.is_mouse_busy == false \
+		and saddle_in_position == false:
+		
 		selected = true
 		game_manager.is_mouse_busy = true
 		
@@ -27,7 +32,13 @@ func _input(event):
 		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 			selected = false
 			game_manager.is_mouse_busy = false
-
+			
+			
+func release_left_mouse_button():
+	var a = InputEventMouseButton.new()
+	a.set_button_index(1)
+	a.set_pressed(false)
+	Input.parse_input_event(a)
 
 
 func _on_mouse_entered():
@@ -35,3 +46,18 @@ func _on_mouse_entered():
 
 func _on_mouse_exited():
 	$Label.hide()
+
+
+# makes saddle satay in place on the bike
+func _on_area_entered(area):
+	print(area.name)
+	var bike_saddle_area
+	if area.name == "SaddleBikeArea2D":
+		self.global_position = Vector2(-7.5, -16.5) #area of saddleBikeCollision on bike
+		saddle_in_position = true
+		release_left_mouse_button()
+		
+		#self.position=bike_saddle_area
+		
+
+
