6b865923919220a7700a43c844a3af2eb4a4276b
javi <javierrcalvete@gmail.com>
2024-06-23 16:42:31 -0300

    made front wheel stay in position on bike/cant click it again

--- scripts/wheel_front.gd
+++ scripts/wheel_front.gd
@@ -1,11 +1,15 @@
 extends Area2D
 @onready var game_manager = %GameManager
 
+var wheel_in_position = false
+
 var selected = false
 
 # variable para saber si se selecciono el collision shape, escucha solos los del collision
 func _on_input_event(viewport, event, shape_idx):
-	if Input.is_action_just_pressed("click") and game_manager.is_mouse_busy == false:
+	if Input.is_action_just_pressed("click") and game_manager.is_mouse_busy == false \
+		and wheel_in_position == false:
+		
 		selected = true
 		game_manager.is_mouse_busy = true
 		
@@ -21,3 +25,31 @@ func _input(event):
 		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 			selected = false
 			game_manager.is_mouse_busy = false
+			
+func release_left_mouse_button():
+	var a = InputEventMouseButton.new()
+	a.set_button_index(1)
+	a.set_pressed(false)
+	Input.parse_input_event(a)
+
+
+func _on_mouse_entered():
+	$Label.show()
+
+func _on_mouse_exited():
+	$Label.hide()
+
+
+# makes saddle stay in place on the bike
+func _on_area_entered(area):
+	print(area.name)
+	
+	if area.name == "FrontWheelArea2D":
+		self.global_position = Vector2(13, 2) #area of saddleBikeCollision on bike
+		print(self.global_position)
+		wheel_in_position = true
+		release_left_mouse_button()
+		
+		#self.position=bike_saddle_area
+		
+
