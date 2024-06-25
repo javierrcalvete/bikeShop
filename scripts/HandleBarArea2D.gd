15979d8b58fd788f96303dd37b3b9fb7b5370a68
javi <javierrcalvete@gmail.com>
2024-06-25 16:49:40 -0300

    added script handlebarArea for global position

---
+++ scripts/HandleBarArea2D.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+var handleBar_position = self.global_position
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(handleBar_position)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
