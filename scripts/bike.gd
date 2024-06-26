1f2e9f35b73ce2fd93476a7f1f4b4dc9e297267f
javi <javierrcalvete@gmail.com>
2024-06-26 16:10:09 -0300

    added backWheel drop zones/color zones functionality

--- scripts/bike.gd
+++ scripts/bike.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var handleBar_area = $HandleBarArea2D
 @onready var saddle_area = $SaddleBikeArea2D
 @onready var frontWheel_area =$FrontWheelArea2D
+@onready var wheelBack_area = $BackWheelArea2D2
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,6 +17,7 @@ func _process(delta):
 	handleBar_area.visible = false
 	saddle_area.visible = false
 	frontWheel_area.visible = false
+	wheelBack_area.visible = false
 	if game_manager.is_mouse_busy:
 # when bikepart grabbed show where to put it with area
 		print(game_manager.name_bikePart_grabbed)
@@ -25,4 +28,6 @@ func _process(delta):
 				saddle_area.visible = true
 			'WheelFront':
 				frontWheel_area.visible = true
+			'WheelBack':
+				wheelBack_area.visible = true
 			
