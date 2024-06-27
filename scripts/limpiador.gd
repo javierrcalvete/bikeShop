2c8dad69aff9418d6c5c7857b4d7111136bfc729
javi <javierrcalvete@gmail.com>
2024-06-26 21:54:22 -0300

    added label/funtionality limpiador

--- scripts/limpiador.gd
+++ scripts/limpiador.gd
@@ -5,11 +5,17 @@ extends Area2D
 
 var selected = false
 
+func _ready():
+	$Label.hide()
+	
+	
+
 # variable para saber si se selecciono el collision shape, escucha solos los del collision
 func _on_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("click") and game_manager.is_mouse_busy == false:
 		selected = true
 		game_manager.is_mouse_busy = true
+		game_manager.name_bikePart_grabbed = self.name
 		
 		
 # lerp agarra 2 valores y lo interpola con la velocidad del 3 valor
@@ -23,3 +29,22 @@ func _input(event):
 		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 			selected = false
 			game_manager.is_mouse_busy = false
+
+
+
+
+#func _on_area_entered(area):
+	#if area.name == "Mud":
+		#$Label.show()
+
+
+#func _on_area_exited(area):
+	#$Label.hide()
+
+
+func _on_mouse_entered():
+	$Label.show()
+
+
+func _on_mouse_exited():
+	$Label.hide()
