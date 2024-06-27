a01025d076fb80e5d9c8c0582d794164121ca067
javi <javierrcalvete@gmail.com>
2024-06-27 15:45:34 -0300

    adde menu buttons/functionalioty

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,12 +1,21 @@
 extends Node
 
-@onready var score_label = $ScoreLabel
-@onready var habldeBarNode=$"../BikeParts/Handlebar"
+@onready var score_label : Label = $ScoreLabel
+@onready var habldeBarNode : Node2D = $"../BikeParts/Handlebar"
+@onready var menu : Control = $Menu
+
+# toggle menu with escape
+func _input(event: InputEvent):
+	if (event.is_action_pressed("ui_cancel")):
+		var menu_visible : bool = menu.visible
+		menu.visible = !menu_visible
+	
+
 var name_bikePart_grabbed
 #@onready var mud = $Mud
 # var for check if mouse is grabbing something
-var is_mouse_busy = false
-var score = 0
+var is_mouse_busy : bool = false
+var score : int = 0
 var clicks_mud = 0
 
 
---
+++ scripts/Menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _ready():
+	self.hide()
+
+
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
